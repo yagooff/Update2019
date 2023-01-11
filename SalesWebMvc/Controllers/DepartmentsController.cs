a32127073245517fefa133e21a482afa0ffafc33
Yago Dias <yago.dias9@gmail.com>
2023-01-10 21:54:53 -0300

    [200~ yago@DESKTOP-DSL23CD MINGW64 ~/source/repos/SalesWebMvc (main) $ git add .
    yago@DESKTOP-DSL23CD MINGW64 ~/source/repos/SalesWebMvc (main)
    $ git commit -m

---
+++ SalesWebMvc/Controllers/DepartmentsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
+using System.Collections.Generic;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Departments> list = new List<Departments>();
+            list.Add(new Departments { Id = 1, Name = "Eletronics " });
+            list.Add(new Departments { Id = 1, Name = "Fashion " });
+
+            return View(list);
+        }
+    }
+}
--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -8,7 +8,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace SalesWebMvc {
+namespace SalesWebMvc.Models.ViewsModels {
     public class Program {
         public static void Main(string[] args) {
             CreateWebHostBuilder(args).Build().Run();
